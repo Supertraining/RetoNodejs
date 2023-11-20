972cb7b0abbdf10307e3f8d476da48b068ed8f92
Maria Clara <mariac.uribe1606@gmail.com>
2023-11-19 22:04:04 -0500

    Task controllers created

--- src/tasks/controllers/task.js
+++ src/tasks/controllers/task.js
@@ -1,6 +1,5 @@
 const TaskService = require('../services/task');
 
-
 class TaskController {
 
   constructor() {
@@ -8,12 +7,65 @@ class TaskController {
   }
 
   getTasks = async (req, res) =>  {
+    try {
+      const tasks = await this.taskService.getTasks();
+      tasks.length === 0 
+      ? res.status(204).send()
+      : res.json(tasks)
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
 
-    const tasks = await this.taskService.getTasks()
-    
-    tasks.length === 0 
-    ? res.status(204).send()
-    : res.json(tasks)
+  saveNewTask = async (req, res) => {
+    try {
+      const { username, date, name, description } = req.body;
+      const newTask = await this.taskService.saveNewTask(username, date, name, description);
+      newTask.length === 0 
+      ? res.status(204).send()
+      : res.json(newTask)
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+  }
+
+  updateTask = async (req, res) => {
+    try {
+      const { id, username, date, name, description } = req.body;
+      const task = await models.Tasks.findByPk(id);
+      if (!task) {
+        return res.status(404).json({ error: 'Task not found' });
+      }
+      await task.update({username, date, name, description});
+      res.json({ message: 'Task updated successfully' });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  upsertTask = async (req, res) => {
+    try {
+      const { id, username, date, name, description } = req.body;  
+      const task = await this.taskService.upsertTask(id, username, date, name, description);
+      task.length === 0 
+      ? res.status(204).send()
+      : res.json(task);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  deleteTask = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const taskToDelete = await this.taskService.deleteTask(id);
+      if (taskToDelete === 0) {
+        return res.status(404).json({ error: 'Task not found' });
+      }
+      res.json({ message: 'Task deleted successfully' });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   }
 
 }
